c155f17d054ce4872eb70b6c8d2a676cb128c85f
Abdullah Reza <air.reza@hotmail.com>
2018-06-29 09:57:35 +0800

    Split Modules

--- MainApp/app.js
+++ MainApp/app.js
@@ -16,16 +16,97 @@
 // 3. Smartlogger Parameters Query - COMPLETE (REZA)
 // 4. CONTROLLING MODBUS DEVICES - COMPLETE (REZA)
 // 5. DATABASE - COMPLETE (REZA)
-//
+// 6. OFFLINE DATA MANAGEMENT - COMPLETE (REZA)
+// 7. AUTO STARTUP NODE SCRIPT - COMPLETE (REZA)
 //
 //**********************************************************************
 
-const meters = require('./getEnergyMeter.js');
-const pvSystem = require('./getPVSystem.js');
+//**********************************************************************
+//
+// NPM MODULES
+//
+//**********************************************************************
+
+const ModbusRTU = require('modbus-serial');
+const firebase = require('firebase');
 
 //**********************************************************************
 //
-// Project Configuration
+// ADDITIONAL MODULES
+//
+//**********************************************************************
+
+const getMeters = require('./getEnergyMeter.js');
+// const pvSystem = require('./getPVSystem.js');
+
+//**********************************************************************
+//
+// INITIAL CONFIGURATIONS: MODBUS-RTU RS-485
+// DIGITAL POWER METER
+//
+//**********************************************************************
+
+var energyMeter = new ModbusRTU();
+energyMeter.connectRTUBuffered('/dev/ttyAMA0', {baudRate: 9600}, function(error, data) {
+	if (error) {
+		console.log(`Cannot initialize serial port: ${error}`);
+	} else if (data) {
+		console.log('Serial port has been successfully initialized', data);
+	}
+});
+
+//**********************************************************************
+//
+// INITIAL CONFIGURATIONS: MODBUS RTU TCP/IP
+// HUAWEI SMARTLOGGER 
+//
+//**********************************************************************
+
+var smartLogger = new ModbusRTU();
+smartLogger.connectTCP("192.168.54.10", {port: 502}, function(error, data) {
+	if (error) {
+		console.log(`Cannot initialize TCP/IP port: ${error}`);
+	} else if (data) {
+		console.log('TCP/IP port has been successfully initialized', data);
+	}
+});
+
+//**********************************************************************
+//
+// INITIAL CONFIGURATIONS: FIREBASE
+// REALTIME DATABASE
+//
+//**********************************************************************
+
+var config = {
+	apiKey: "AIzaSyBpzfNgXNeKz8X0CQBG29W3R8CsXVh8pwI",
+    authDomain: "source-57d08.firebaseapp.com",
+    databaseURL: "https://source-57d08.firebaseio.com",
+    projectId: "source-57d08",
+    storageBucket: "source-57d08.appspot.com",
+};
+firebase.initializeApp(config);
+var projectDatabase = firebase.database();
+
+//**********************************************************************
+//
+// CHECK INTERNET CONNECTION
+//
+//**********************************************************************
+
+const connectedRef = firebase.database().ref(".info/connected");
+connectedRef.on("value", function(snap) {
+    if (snap.val() === true) {
+        connectState = snap.val();
+    } else {
+        connectState = false;
+        console.log(typeof connectState);
+    }
+});
+
+//**********************************************************************
+//
+// PROJECT CONFIGURATION: DIGITAL POWER METER
 //
 //**********************************************************************
 
@@ -34,7 +115,15 @@ const numberOfInverters = 19;
 
 //**********************************************************************
 //
-// Entry point of the app
+// ERROR COUNTER TO HANDLE DATA PACKET LOSS
+//
+//**********************************************************************
+
+var errorCount = null;
+
+//**********************************************************************
+//
+// ENTRY POINT OF THE APP
 //
 //**********************************************************************
 
@@ -43,23 +132,52 @@ console.log('"He who has begun is half done: DARE TO KNOW!" - Horace');
 
 //**********************************************************************
 //
-// MAIN LOOP
+// MAIN FUNCTION
 //
 //**********************************************************************
 
-setInterval(() => {
-	// Get energy parameters
-    meters.getMeters(numberOfMeters).then(energyParameters => {
-		//let energyParameters = energyMeterArray;
-		console.log(`energyParameters:, ${energyParameters}`);
-	}).catch(error => {
-		console.log(error.message)
-	});
-	// Get PV system parameters
-	pvSystem.getPVSystemParameters(numberOfInverters).then(pvSystemArray => {
-		//let pvSystemParameters = pvSystemArray;
-		console.log(`pvSystemParameters:', ${pvSystemArray}`);
-	}).catch(error => {
+const main = async () => {
+	try {
+		console.log('Inside main');
+		getMeters.getEnergyParameters(numberOfMeters);
+		// console.log('Get PV System');
+		// console.log('Zero export control');
+		// console.log('Push data to cloud');
+	} catch (error) {
 		console.log(error.message);
-	})
-}, 30000);
+	} finally {
+		console.log('Done');
+		await delay(5000);
+		main();
+	}
+};
+
+//**********************************************************************
+//
+// DELAY
+//
+//**********************************************************************
+
+const delay = microSecond => new Promise(resolve => setTimeout(resolve, microSecond));
+
+delay(30000).then(() => {
+	main();
+});
+// main();
+
+// (() => {
+// 	// Get energy parameters
+//     meters.getMeters(numberOfMeters).then(energyParameters => {
+// 		//let energyParameters = energyMeterArray;
+// 		console.log(`energyParameters:, ${energyParameters}`);
+// 	}).catch(error => {
+// 		console.log(error.message)
+// 	});
+// 	// Get PV system parameters
+// 	pvSystem.getPVSystemParameters(numberOfInverters).then(pvSystemArray => {
+// 		//let pvSystemParameters = pvSystemArray;
+// 		console.log(`pvSystemParameters:', ${pvSystemArray}`);
+// 	}).catch(error => {
+// 		console.log(error.message);
+// 	})
+// }, 30000);
