5a0b7fea15d0d7ae23be5706094a061d532e5c0a
Abdullah Reza <air.reza@hotmail.com>
2018-08-13 10:43:22 +0800

    Amended on 13/08/2018

--- MainApp/controlPowerOutput.js
+++ MainApp/controlPowerOutput.js
@@ -7,38 +7,26 @@
 // Smartlogger ID
 const smartloggerId = 0;
 
+// Read only register for AC power output from PV
+const activePowerRegister_R = 40525;
+
 // Register (RW) for AC power supply from PV
 const activeAdjustmentRegister_RW = 40424;
 
 //**********************************************************************
 //
-// INSTALLED PV SYSTEM CAPACITY
+// INSTALLED PV SYSTEM 
 //
 //**********************************************************************
 
 // Max. capacity of the installed system
 const maxPVoutput = 7326;
 
+// Zero export ceiling rate
+const zeroExportLimit = 6593;
+
 // Safety buffer 10%
 const bufferOutput = 0.10;
-
-//**********************************************************************
-//
-// GAIN FACTOR AS DESCRIBED BY THE SMARTLOGGER USER MANUAL
-//
-//**********************************************************************
-
-const GAIN_FACTOR = 10;
-
-//**********************************************************************
-//
-// MAXIMUM VALUE OF THE POWER REGISTER
-// SINCE POWER REGISTER REPRESENTS UInt32 VALUE, MAX. VALUE REPRESENTS
-// ENERGY EXPORT BACK TO THE GRID
-//
-//**********************************************************************
-
-const NEGATIVE_POWER = 4294967;
 
 const adjustSolarOutput = async (smartlogger, intakeTNB, smartloggerParameters) => {
     try {
@@ -46,22 +34,21 @@ const adjustSolarOutput = async (smartlogger, intakeTNB, smartloggerParameters) 
         await smartlogger.setTimeout(500);
 
         totalBuildingLoad = smartloggerParameters.totalPVacPower + intakeTNB;
-        if (totalBuildingLoad > NEGATIVE_POWER) {
-            let adjustOutput = maxPVoutput * 0.01;
-			await smartlogger.writeRegisters(activeAdjustmentRegister_RW, [0, adjustOutput]);
+        if (totalBuildingLoad > 4294967) {
+			await smartlogger.writeRegisters(activeAdjustmentRegister_RW, [0, 1000]);
             activeAdjustment = await smartlogger.readHoldingRegisters(activeAdjustmentRegister_RW, 2);
-            smartloggerParameters.activeAdjustment = activeAdjustment.buffer.readUInt32BE(0) / GAIN_FACTOR;
+            smartloggerParameters.activeAdjustment = activeAdjustment.buffer.readUInt32BE(0) / 10;
             smartloggerParameters.totalBuildingLoad = totalBuildingLoad;
-		} else if (totalBuildingLoad > ((maxPVoutput / GAIN_FACTOR) * (1 + bufferOutput))) {
+		} else if (totalBuildingLoad > ((maxPVoutput / 10) * (1 + bufferOutput))) {
             await smartlogger.writeRegisters(activeAdjustmentRegister_RW, [0, maxPVoutput]);
             activeAdjustment = await smartlogger.readHoldingRegisters(activeAdjustmentRegister_RW, 2);
-            smartloggerParameters.activeAdjustment = activeAdjustment.buffer.readUInt32BE(0) / GAIN_FACTOR;
+            smartloggerParameters.activeAdjustment = activeAdjustment.buffer.readUInt32BE(0) / 10;
             smartloggerParameters.totalBuildingLoad = totalBuildingLoad;
         } else {
-            let adjustOutput = totalBuildingLoad * (1 - bufferOutput) * GAIN_FACTOR; 
+            let adjustOutput = totalBuildingLoad * (1 - bufferOutput) * 10; 
             await smartlogger.writeRegisters(activeAdjustmentRegister_RW, [0, adjustOutput]);
             activeAdjustment = await smartlogger.readHoldingRegisters(activeAdjustmentRegister_RW, 2);
-            smartloggerParameters.activeAdjustment = activeAdjustment.buffer.readUInt32BE(0) / GAIN_FACTOR;
+            smartloggerParameters.activeAdjustment = activeAdjustment.buffer.readUInt32BE(0) / 10;
             smartloggerParameters.totalBuildingLoad = totalBuildingLoad;
         }
 	} catch (error) {
