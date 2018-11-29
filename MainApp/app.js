a53b375df80447fb7730aedbc18fdd743ba6d013
Ryan Oh <ryan.oh@plus-solar.com.my>
2018-11-29 02:24:23 +0000

    Replace app.js

--- MainApp/app.js
+++ MainApp/app.js
@@ -29,7 +29,9 @@
 //**********************************************************************
 
 const ModbusRTU = require("modbus-serial");
-const firebase = require("firebase");
+const awsIot = require("aws-iot-device-sdk");
+const levelStore = require("mqtt-level-store");
+const levelManager = levelStore("./store");
 
 //**********************************************************************
 //
@@ -42,6 +44,7 @@ const getSolar = require("./getPVSystem.js");
 const solarOutputControl = require("./controlPowerOutput.js");
 const manageDatabase = require("./manageDatabase.js");
 const performanceData = require("./processData.js");
+const payload = require("./payload.js").payload;
 
 //**********************************************************************
 //
@@ -77,76 +80,55 @@ smartlogger.connectTCP("10.10.0.61", {port: 502}, function(error, data) {
 
 //**********************************************************************
 //
-// INITIAL CONFIGURATIONS: FIREBASE
+// INITIAL CONFIGURATIONS: AWS
 // REALTIME DATABASE
 //
 //**********************************************************************
 
-var config = {
-	apiKey: "AIzaSyBpzfNgXNeKz8X0CQBG29W3R8CsXVh8pwI",
-	authDomain: "source-57d08.firebaseapp.com",
-	databaseURL: "https://source-57d08.firebaseio.com",
-	projectId: "source-57d08",
-	storageBucket: "source-57d08.appspot.com",
-};
-
-/*
-var config = {
-	apiKey: "AIzaSyB-6YYD6W9-yN-E4wvFNpxtEL6IHX9nVuQ",
-	authDomain: "prototype-test-development.firebaseapp.com",
-	databaseURL: "https://prototype-test-development.firebaseio.com",
-	projectId: "prototype-test-development",
-	storageBucket: "prototype-test-development.appspot.com"
-};
-*/ 
-
-firebase.initializeApp(config);
-var projectDatabase = firebase.database();
-
-//**********************************************************************
-//
-// CHECK INTERNET CONNECTION
-//
-//**********************************************************************
-
-var connectionStatus = null;
-const connectedRef = firebase.database().ref(".info/connected");
-connectedRef.on("value", function(snap) {
-	if (snap.val()) {
-		connectionStatus = snap.val();
-	} else {
-		connectionStatus = false;
-	}
+const path = "./certs/";
+const device = awsIot.device({
+  incomingStore: levelManager.incoming,
+  outgoingStore: levelManager.outgoing,
+  keyPath: `${path}449da88e49-private.pem.key`,
+  certPath: `${path}449da88e49-certificate.pem.crt`,
+  caPath: `${path}AmazonRootCA1.pem`,
+  clientId: "MyRaspberryPi",
+  host: "a3j1bzslspz95c-ats.iot.ap-southeast-1.amazonaws.com",
+  offlineQueueing: false,
+  port: 8883
 });
 
 //**********************************************************************
 //
-// CHECK BASELINE STATUS
+// 					CHECK INTERNET CONNECTION
 //
 //**********************************************************************
 
-var baselineControl = null;
-let baselineControlRef = projectDatabase.ref('/Customer/ThongGuanLot48/DatalogControl/');
-baselineControlRef.once('value').then(function(snapshot) {
-	baselineControl = snapshot.val().Baseline;
-}).catch(error => {
-	console.log(error);
+device.on("connect", function() {
+  console.log("connected");
 });
 
-//**********************************************************************
-//
-// CHECK CONFIGURATIONS OF PV SYSTEM
-//
-//**********************************************************************
+device.on("message", function(topic, payload) {
+  console.log("receiving message");
+  //console.log('message', topic, payload.toString());
+});
 
-var solarSystemConfig = new Object;
-let configRef = projectDatabase.ref('/Customer/ThongGuanLot48/Config/');
-configRef.once('value', function(snapshot) {
-	snapshot.forEach(function(childSnapshot) {
-		let key = childSnapshot.key;
-		let value = childSnapshot.val();
-		solarSystemConfig[key] = value;
-	});
+device.on("close", function() {
+  console.log("disconnected");
+  //console.log('disconnected', arguments);
+});
+
+device.on("error", function() {
+  //console.log('error', arguments);
+});
+
+device.on("reconnect", function() {
+  console.log("reconnect...");
+  //console.log('reconnect', arguments);
+});
+
+device.on("timeout", function() {
+  //console.log('timeout', arguments);
 });
 
 //**********************************************************************
@@ -157,6 +139,10 @@ configRef.once('value', function(snapshot) {
 
 const numberOfMeters = 1;
 const numberOfInverters = 19;
+var pubOpts = { qos: 1 };
+var interval = null;
+var timeInterval = 10; // seconds
+var dat = new Date();
 
 //**********************************************************************
 //
@@ -171,14 +157,85 @@ const main = async () => {
 		let allInverterParameters = await getSolar.getInverters(smartlogger, numberOfInverters);
 		let allPVParameters = await getSolar.getSmartlogger(smartlogger);
 		let limitPVOutput = await solarOutputControl.adjustSolarOutput(smartlogger, allMeterParameters.Meter1.intakeTNB, allPVParameters.SmartLogger);
-		let systemPerformance = await performanceData.performanceParameters(allMeterParameters.Meter1, allPVParameters.SmartLogger, solarSystemConfig);
-		console.log("IntakeTNBPower:", allMeterParameters.Meter1.intakeTNB);
-		console.log("dailyMaxDemand:", systemPerformance.DailyReadings.dailyMaxDemand);
-		manageDatabase.manageData(projectDatabase, connectionStatus, baselineControl, allMeterParameters, allPVParameters, systemPerformance);
+		//let systemPerformance = await performanceData.performanceParameters(allMeterParameters.Meter1, allPVParameters.SmartLogger, solarSystemConfig);
+		//manageDatabase.manageData(projectDatabase, connectionStatus, baselineControl, allMeterParameters, allPVParameters, systemPerformance);
+
+		//console.log(payload);
+		//console.log(payload.logger);
+		//console.log(payload.inverter);
+		console.log(payload.powerMeter);
+		
+/*
+		payload.logger.locationId = "xxxxx-xxx-xxx-xxx-l"; // take from admin panel
+		payload.logger.lotId = "xxxxx-xxx-xxx-xxx-lo"; // take from admin panel
+		payload.logger.companyId = "xxxxx-xxx-xxx-xxx-c"; // take from admin panel
+		payload.logger.deviceId = "xxxxx-xxx-xxx-xxx-d"; // take from admin panel
+		payload.logger.createdAt = dat;
+		payload.logger.updatedAt = dat;
+		payload.logger.powerAdjustment = allPVParameters.SmartLogger.activeAdjustment;
+		payload.logger.ambientTemp = allPVParameters.SmartLogger.ambientTemp;
+		payload.logger.moduleTemp = allPVParameters.SmartLogger.moduleTemp;
+		payload.logger.irrSensor = allPVParameters.SmartLogger.IRRsensor;
+		payload.logger.totalBuildingLoad = allPVParameters.SmartLogger.totalBuildingLoad;
+		payload.logger.activePower = 8;
+		payload.logger.windSpeed = allPVParameters.SmartLogger.windSpeed;
+*/		
+/*
+  const totalInverter = 5;
+  for (let i = 0; i < totalInverter; i++) {
+    payload.inverter[i].createdAt = dat;
+    payload.inverter[i].updatedAt = dat;
+    payload.inverter[i].ref = i + 1;
+    payload.inverter[i].effInverter = allPVParameters.inverter1.effInverter;;
+    payload.inverter[i].acPower = allPVParameters.inverter1.inverterACpower;
+    payload.inverter[i].dcPower = allPVParameters.inverter1.inverterDCpower;
+    payload.inverter[i].pvCurrent = allPVParameters.inverter1.inverterPV1current;
+    payload.inverter[i].pcVoltage = allPVParameters.inverter1.inverterPV1voltage;
+    payload.inverter[i].mpptPower = allPVParameters.inverter1.MPPT1power;
+  }
+*/
+/*
+  const totalPwerMeter = 1;
+  for (let j = 0; j < totalPwerMeter; j++) {
+    payload.powerMeter[j].createdAt = dat;
+    payload.powerMeter[j].updatedAt = dat;
+    payload.powerMeter[j].ref = j + 1;
+    payload.powerMeter[j].activeEnergy = allMeterParameters.Meter1.activeEnergy;
+    payload.powerMeter[j].activePower = allMeterParameters.Meter1.intakeTNB;
+    payload.powerMeter[j].maxDemand = allMeterParameters.Meter1.maxDemand;
+    payload.powerMeter[j].voltage = {
+      "1": 2,
+      "2": 2,
+      "3": 3
+    };
+    payload.powerMeter[j].current = {
+      "1": 2,
+      "2": 2,
+      "3": 3
+    };
+    payload.powerMeter[j].power = {
+      "1": 2,
+      "2": 2,
+      "3": 3
+    };
+    payload.powerMeter[j].mDlevel = {
+      "1": 1,
+      "2": 2
+    };
+  }
+  //console.log(payload);
+  //console.log(payload.logger);
+  //console.log(payload.inverter[inverterNumber].inverterPV1voltage);
+  //console.log(payload.inverter[1].inverterP1current);
+  //console.log(payload.powerMeter);
+	//console.log(payload.inverter[inverterNumber].effInverter);
+*/
+  device.publish("publish/reading", JSON.stringify(payload), pubOpts);
+  
 	} catch (error) {
 		console.log(error.message);
 	} finally {
-		await delay(60000);
+		await delay(5000);
 		console.log('***** ITERATION COMPLETE *****');
 		main();
 	}
@@ -192,7 +249,7 @@ const main = async () => {
 
 const delay = microSecond => new Promise(resolve => setTimeout(resolve, microSecond));
 
-delay(20000).then(() => {
+delay(10000).then(() => {
 	console.log("Start app.js")
 	main();
 });
