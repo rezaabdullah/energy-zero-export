9403eb9f0ce258691c01e8a4b99d011c11bf4c5c
Ryan Oh <ryan.oh@plus-solar.com.my>
2018-11-29 02:26:19 +0000

    Upload New File

---
+++ MainApp/payload.js
@@ -0,0 +1,686 @@
+module.exports.payload = {
+    logger: {
+      locationId: "8574723183",
+      lotId: "lot123",
+      companyId: "com-123",
+      deviceId: "device123",
+      createdAt: "2018-10-23T06:46:48.782Z",
+      updatedAt: "2018-10-23T06:46:48.782Z",
+      powerAdjustment: null,
+      ambientTemp: null,
+      moduleTemp: null,
+      irrSensor: null,
+      totalBuildingLoad: null,
+      activePower: null,
+      windSpeed: null
+    },
+  
+    inverter: [
+      {
+        createdAt: "2018-10-23T06:46:48.782Z",
+        updatedAt: "2018-10-23T06:46:48.782Z",
+        ref: 1,
+        effInverter: null,
+        acPower: null,
+        dcPower: null,
+        pvCurrent: {
+          1: null,
+          2: null,
+          3: null,
+          4: null,
+          5: null,
+          6: null
+        },
+        pcVoltage: {
+          1: null,
+          2: null,
+          3: null,
+          4: null,
+          5: null,
+          6: null
+        },
+        mpptPower: {
+          1: null,
+          2: null,
+          3: null
+        }
+      },
+      {
+        createdAt: "2018-10-23T06:46:48.782Z",
+        updatedAt: "2018-10-23T06:46:48.782Z",
+        ref: 2,
+        effInverter: null,
+        acPower: null,
+        dcPower: null,
+        pvCurrent: {
+          1: null,
+          2: null,
+          3: null,
+          4: null,
+          5: null,
+          6: null
+        },
+        pcVoltage: {
+          1: null,
+          2: null,
+          3: null,
+          4: null,
+          5: null,
+          6: null
+        },
+        mpptPower: {
+          1: null,
+          2: null,
+          3: null
+        }
+      },
+      {
+        createdAt: "2018-10-23T06:46:48.782Z",
+        updatedAt: "2018-10-23T06:46:48.782Z",
+        ref: 3,
+        effInverter: null,
+        acPower: null,
+        dcPower: null,
+        pvCurrent: {
+          1: null,
+          2: null,
+          3: null,
+          4: null,
+          5: null,
+          6: null
+        },
+        pcVoltage: {
+          1: null,
+          2: null,
+          3: null,
+          4: null,
+          5: null,
+          6: null
+        },
+        mpptPower: {
+          1: null,
+          2: null,
+          3: null
+        }
+      },
+      {
+        createdAt: "2018-10-23T06:46:48.782Z",
+        updatedAt: "2018-10-23T06:46:48.782Z",
+        ref: 4,
+        effInverter: null,
+        acPower: null,
+        dcPower: null,
+        pvCurrent: {
+          1: null,
+          2: null,
+          3: null,
+          4: null,
+          5: null,
+          6: null
+        },
+        pcVoltage: {
+          1: null,
+          2: null,
+          3: null,
+          4: null,
+          5: null,
+          6: null
+        },
+        mpptPower: {
+          1: null,
+          2: null,
+          3: null
+        }
+      },
+      {
+        createdAt: "2018-10-23T06:46:48.782Z",
+        updatedAt: "2018-10-23T06:46:48.782Z",
+        ref: 5,
+        effInverter: null,
+        acPower: null,
+        dcPower: null,
+        pvCurrent: {
+          1: null,
+          2: null,
+          3: null,
+          4: null,
+          5: null,
+          6: null
+        },
+        pcVoltage: {
+          1: null,
+          2: null,
+          3: null,
+          4: null,
+          5: null,
+          6: null
+        },
+        mpptPower: {
+          1: null,
+          2: null,
+          3: null
+        }
+      },
+      {
+        createdAt: "2018-10-23T06:46:48.782Z",
+        updatedAt: "2018-10-23T06:46:48.782Z",
+        ref: 6,
+        effInverter: null,
+        acPower: null,
+        dcPower: null,
+        pvCurrent: {
+          1: null,
+          2: null,
+          3: null,
+          4: null,
+          5: null,
+          6: null
+        },
+        pcVoltage: {
+          1: null,
+          2: null,
+          3: null,
+          4: null,
+          5: null,
+          6: null
+        },
+        mpptPower: {
+          1: null,
+          2: null,
+          3: null
+        }
+      },
+      {
+        createdAt: "2018-10-23T06:46:48.782Z",
+        updatedAt: "2018-10-23T06:46:48.782Z",
+        ref: 7,
+        effInverter: null,
+        acPower: null,
+        dcPower: null,
+        pvCurrent: {
+          1: null,
+          2: null,
+          3: null,
+          4: null,
+          5: null,
+          6: null
+        },
+        pcVoltage: {
+          1: null,
+          2: null,
+          3: null,
+          4: null,
+          5: null,
+          6: null
+        },
+        mpptPower: {
+          1: null,
+          2: null,
+          3: null
+        }
+      },
+      {
+        createdAt: "2018-10-23T06:46:48.782Z",
+        updatedAt: "2018-10-23T06:46:48.782Z",
+        ref: 8,
+        effInverter: null,
+        acPower: null,
+        dcPower: null,
+        pvCurrent: {
+          1: null,
+          2: null,
+          3: null,
+          4: null,
+          5: null,
+          6: null
+        },
+        pcVoltage: {
+          1: null,
+          2: null,
+          3: null,
+          4: null,
+          5: null,
+          6: null
+        },
+        mpptPower: {
+          1: null,
+          2: null,
+          3: null
+        }
+      },
+      {
+        createdAt: "2018-10-23T06:46:48.782Z",
+        updatedAt: "2018-10-23T06:46:48.782Z",
+        ref: 9,
+        effInverter: null,
+        acPower: null,
+        dcPower: null,
+        pvCurrent: {
+          1: null,
+          2: null,
+          3: null,
+          4: null,
+          5: null,
+          6: null
+        },
+        pcVoltage: {
+          1: null,
+          2: null,
+          3: null,
+          4: null,
+          5: null,
+          6: null
+        },
+        mpptPower: {
+          1: null,
+          2: null,
+          3: null
+        }
+      },
+      {
+        createdAt: "2018-10-23T06:46:48.782Z",
+        updatedAt: "2018-10-23T06:46:48.782Z",
+        ref: 10,
+        effInverter: null,
+        acPower: null,
+        dcPower: null,
+        pvCurrent: {
+          1: null,
+          2: null,
+          3: null,
+          4: null,
+          5: null,
+          6: null
+        },
+        pcVoltage: {
+          1: null,
+          2: null,
+          3: null,
+          4: null,
+          5: null,
+          6: null
+        },
+        mpptPower: {
+          1: null,
+          2: null,
+          3: null
+        }
+      },
+      {
+        createdAt: "2018-10-23T06:46:48.782Z",
+        updatedAt: "2018-10-23T06:46:48.782Z",
+        ref: 11,
+        effInverter: null,
+        acPower: null,
+        dcPower: null,
+        pvCurrent: {
+          1: null,
+          2: null,
+          3: null,
+          4: null,
+          5: null,
+          6: null
+        },
+        pcVoltage: {
+          1: null,
+          2: null,
+          3: null,
+          4: null,
+          5: null,
+          6: null
+        },
+        mpptPower: {
+          1: null,
+          2: null,
+          3: null
+        }
+      },
+      {
+        createdAt: "2018-10-23T06:46:48.782Z",
+        updatedAt: "2018-10-23T06:46:48.782Z",
+        ref: 12,
+        effInverter: null,
+        acPower: null,
+        dcPower: null,
+        pvCurrent: {
+          1: null,
+          2: null,
+          3: null,
+          4: null,
+          5: null,
+          6: null
+        },
+        pcVoltage: {
+          1: null,
+          2: null,
+          3: null,
+          4: null,
+          5: null,
+          6: null
+        },
+        mpptPower: {
+          1: null,
+          2: null,
+          3: null
+        }
+      },
+      {
+        createdAt: "2018-10-23T06:46:48.782Z",
+        updatedAt: "2018-10-23T06:46:48.782Z",
+        ref: 13,
+        effInverter: null,
+        acPower: null,
+        dcPower: null,
+        pvCurrent: {
+          1: null,
+          2: null,
+          3: null,
+          4: null,
+          5: null,
+          6: null
+        },
+        pcVoltage: {
+          1: null,
+          2: null,
+          3: null,
+          4: null,
+          5: null,
+          6: null
+        },
+        mpptPower: {
+          1: null,
+          2: null,
+          3: null
+        }
+      },
+      {
+        createdAt: "2018-10-23T06:46:48.782Z",
+        updatedAt: "2018-10-23T06:46:48.782Z",
+        ref: 14,
+        effInverter: null,
+        acPower: null,
+        dcPower: null,
+        pvCurrent: {
+          1: null,
+          2: null,
+          3: null,
+          4: null,
+          5: null,
+          6: null
+        },
+        pcVoltage: {
+          1: null,
+          2: null,
+          3: null,
+          4: null,
+          5: null,
+          6: null
+        },
+        mpptPower: {
+          1: null,
+          2: null,
+          3: null
+        }
+      },
+      {
+        createdAt: "2018-10-23T06:46:48.782Z",
+        updatedAt: "2018-10-23T06:46:48.782Z",
+        ref: 15,
+        effInverter: null,
+        acPower: null,
+        dcPower: null,
+        pvCurrent: {
+          1: null,
+          2: null,
+          3: null,
+          4: null,
+          5: null,
+          6: null
+        },
+        pcVoltage: {
+          1: null,
+          2: null,
+          3: null,
+          4: null,
+          5: null,
+          6: null
+        },
+        mpptPower: {
+          1: null,
+          2: null,
+          3: null
+        }
+      },
+      {
+        createdAt: "2018-10-23T06:46:48.782Z",
+        updatedAt: "2018-10-23T06:46:48.782Z",
+        ref: 16,
+        effInverter: null,
+        acPower: null,
+        dcPower: null,
+        pvCurrent: {
+          1: null,
+          2: null,
+          3: null,
+          4: null,
+          5: null,
+          6: null
+        },
+        pcVoltage: {
+          1: null,
+          2: null,
+          3: null,
+          4: null,
+          5: null,
+          6: null
+        },
+        mpptPower: {
+          1: null,
+          2: null,
+          3: null
+        }
+      },
+      {
+        createdAt: "2018-10-23T06:46:48.782Z",
+        updatedAt: "2018-10-23T06:46:48.782Z",
+        ref: 17,
+        effInverter: null,
+        acPower: null,
+        dcPower: null,
+        pvCurrent: {
+          1: null,
+          2: null,
+          3: null,
+          4: null,
+          5: null,
+          6: null
+        },
+        pcVoltage: {
+          1: null,
+          2: null,
+          3: null,
+          4: null,
+          5: null,
+          6: null
+        },
+        mpptPower: {
+          1: null,
+          2: null,
+          3: null
+        }
+      },
+      {
+        createdAt: "2018-10-23T06:46:48.782Z",
+        updatedAt: "2018-10-23T06:46:48.782Z",
+        ref: 18,
+        effInverter: null,
+        acPower: null,
+        dcPower: null,
+        pvCurrent: {
+          1: null,
+          2: null,
+          3: null,
+          4: null,
+          5: null,
+          6: null
+        },
+        pcVoltage: {
+          1: null,
+          2: null,
+          3: null,
+          4: null,
+          5: null,
+          6: null
+        },
+        mpptPower: {
+          1: null,
+          2: null,
+          3: null
+        }
+      },
+      {
+        createdAt: "2018-10-23T06:46:48.782Z",
+        updatedAt: "2018-10-23T06:46:48.782Z",
+        ref: 19,
+        effInverter: null,
+        acPower: null,
+        dcPower: null,
+        pvCurrent: {
+          1: null,
+          2: null,
+          3: null,
+          4: null,
+          5: null,
+          6: null
+        },
+        pcVoltage: {
+          1: null,
+          2: null,
+          3: null,
+          4: null,
+          5: null,
+          6: null
+        },
+        mpptPower: {
+          1: null,
+          2: null,
+          3: null
+        }
+      },
+      {
+        createdAt: "2018-10-23T06:46:48.782Z",
+        updatedAt: "2018-10-23T06:46:48.782Z",
+        ref: 20,
+        effInverter: null,
+        acPower: null,
+        dcPower: null,
+        pvCurrent: {
+          1: null,
+          2: null,
+          3: null,
+          4: null,
+          5: null,
+          6: null
+        },
+        pcVoltage: {
+          1: null,
+          2: null,
+          3: null,
+          4: null,
+          5: null,
+          6: null
+        },
+        mpptPower: {
+          1: null,
+          2: null,
+          3: null
+        }
+      },
+  
+    ],
+  
+    powerMeter: [
+      {
+        createdAt: "2018-10-23T06:46:48.782Z",
+        updatedAt: "2018-10-23T06:46:48.782Z",
+        ref: 1,
+        activeEnergy: null,
+        activePower: null,
+        voltage: {
+          1: null,
+          2: null,
+          3: null
+        },
+        current: {
+          1: null,
+          2: null,
+          3: null
+        },
+        power: {
+          1: null,
+          2: null,
+          3: null
+        },
+        maxDemand: null,
+        mDlevel: {
+          1: null,
+          2: null
+        }
+      },
+      {
+        createdAt: "2018-10-23T06:46:48.782Z",
+        updatedAt: "2018-10-23T06:46:48.782Z",
+        ref: 2,
+        activeEnergy: null,
+        activePower: null,
+        voltage: {
+          1: null,
+          2: null,
+          3: null
+        },
+        current: {
+          1: null,
+          2: null,
+          3: null
+        },
+        power: {
+          1: null,
+          2: null,
+          3: null
+        },
+        maxDemand: null,
+        mDlevel: {
+          1: null,
+          2: null
+        }
+      },
+      {
+        createdAt: "2018-10-23T06:46:48.782Z",
+        updatedAt: "2018-10-23T06:46:48.782Z",
+        ref: 3,
+        activeEnergy: null,
+        activePower: null,
+        voltage: {
+          1: null,
+          2: null,
+          3: null
+        },
+        current: {
+          1: null,
+          2: null,
+          3: null
+        },
+        power: {
+          1: null,
+          2: null,
+          3: null
+        },
+        maxDemand: null,
+        mDlevel: {
+          1: null,
+          2: null
+        }
+      }
+    ]
+  };
+  
